7096106d45f5ca15be5eb27f620788073c4218fd
Alon Krymgand <me@alon.kr>
2023-04-26 14:23:24 +0300

    Crawlers are now more error resistent

--- cccrawl/crawlers/codeforces.py
+++ cccrawl/crawlers/codeforces.py
@@ -1,25 +1,39 @@
-from cccrawl.crawlers.base import Crawler
+from logging import getLogger
+
+from httpx import HTTPError
+
+from cccrawl.crawlers.base import Crawler, retry
+from cccrawl.crawlers.error import CrawlerError
 from cccrawl.models.solution import SolutionUid
 from cccrawl.models.user import UserConfig
 
+logger = getLogger(__name__)
+
 
 class CodeforcesCrawler(Crawler):
-
+    @retry(exception=HTTPError, start_sleep=5, fail_factor=2)
     async def crawl(self, config: UserConfig) -> set[SolutionUid]:
-        url = f'https://codeforces.com/api/user.status?handle={config.codeforces}&from=1'
+        logger.info("Started crawling Codeforces user %s", config.codeforces)
+        url = (
+            f"https://codeforces.com/api/user.status?handle={config.codeforces}&from=1"
+        )
         response = await self._client.get(url)
+
+        if response.status_code == 400:
+            raise CrawlerError("Can not crawl Codeforces user %s", config.codeforces)
         response.raise_for_status()
-        submissions = response.json().get('result', [])
+
+        submissions = response.json().get("result", [])
         return {
-            self.generate_problem_uid(submission['problem'])
+            self.generate_problem_uid(submission["problem"])
             for submission in submissions
-            if submission['verdict'] == 'OK'
+            if submission["verdict"] == "OK"
         }
 
     @staticmethod
     def generate_problem_uid(problem) -> SolutionUid:
-        contest_id = int(problem['contestId'])
-        problem_id = problem['index']
-        contest_type = 'gym' if contest_id > 100_000 else 'contest'
-        url = f'https://codeforces.com/{contest_type}/{contest_id}/problem/{problem_id}'
+        contest_id = int(problem["contestId"])
+        problem_id = problem["index"]
+        contest_type = "gym" if contest_id > 100_000 else "contest"
+        url = f"https://codeforces.com/{contest_type}/{contest_id}/problem/{problem_id}"
         return url
--- cccrawl/crawlers/cses.py
+++ cccrawl/crawlers/cses.py
@@ -1,20 +1,26 @@
-from bs4 import BeautifulSoup
+from logging import getLogger
 
-from cccrawl.crawlers.base import Crawler
+from bs4 import BeautifulSoup
+from httpx import HTTPError
+
+from cccrawl.crawlers.base import Crawler, retry
+from cccrawl.crawlers.error import CrawlerError
 from cccrawl.models.solution import SolutionUid
 from cccrawl.models.user import UserConfig
 
+logger = getLogger(__name__)
+
 
 class CsesCrawler(Crawler):
-
+    @retry(exception=HTTPError, start_sleep=5, fail_factor=2)
     async def crawl(self, config: UserConfig) -> set[SolutionUid]:
-        url = f'https://cses.fi/problemset/user/{config.cses}/'
+        logger.info("Started crawling CSES user %s", config.cses)
+        url = f"https://cses.fi/problemset/user/{config.cses}/"
         response = await self._client.get(url)
         response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'lxml')
-        table = soup.find('table')
-        solved_tags = table.find_all('a', {'class': 'full'})
-        return {
-            'https://cses.fi' + a_tag['href'][:-1]
-            for a_tag in solved_tags
-        }
+        soup = BeautifulSoup(response.text, "lxml")
+        table = soup.find("table")
+        if table is None:
+            raise CrawlerError(f"CSES user {config.cses} does not exist")
+        solved_tags = table.find_all("a", {"class": "full"})
+        return {"https://cses.fi" + a_tag["href"][:-1] for a_tag in solved_tags}
