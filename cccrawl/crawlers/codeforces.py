f0323c6c02fa0ef8c03eeb382afb01614210998e
Alon Krymgand <me@alon.kr>
2023-07-21 22:02:56 +0300

    Updated codeforces crawler to new format

--- cccrawl/crawlers/codeforces.py
+++ cccrawl/crawlers/codeforces.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 from logging import getLogger
 
 from httpx import HTTPError
 
 from cccrawl.crawlers.base import Crawler, retry
 from cccrawl.crawlers.error import CrawlerError
-from cccrawl.models.solution import SolutionUid
+from cccrawl.models.submission import CrawledSubmission, SubmissionVerdict
 from cccrawl.models.user import UserConfig
 
 logger = getLogger(__name__)
@@ -12,32 +14,40 @@ logger = getLogger(__name__)
 
 class CodeforcesCrawler(Crawler):
     @retry(exception=HTTPError, start_sleep=5, fail_factor=2)
-    async def crawl(self, config: UserConfig) -> set[SolutionUid]:
-        if config.codeforces is None:
+    async def crawl(self, config: UserConfig) -> list[CrawledSubmission]:
+        if handle := config.codeforces is None:
             logger.info("No available Codeforces user, skipping.")
-            return set()
+            return list()
 
-        logger.info("Started crawling Codeforces user %s", config.codeforces)
-        url = (
-            f"https://codeforces.com/api/user.status?handle={config.codeforces}&from=1"
-        )
+        logger.info("Started crawling Codeforces handle '%s'", handle)
+        url = f"https://codeforces.com/api/user.status?handle={handle}&from=1"
         response = await self._client.get(url)
 
         if response.status_code == 400:
-            raise CrawlerError("Can not crawl Codeforces user %s", config.codeforces)
+            raise CrawlerError(
+                "Can not crawl Codeforces user '%s': %s",
+                config.codeforces,
+                str(response.content),
+            )
         response.raise_for_status()
 
         submissions = response.json().get("result", [])
-        return {
-            self.generate_problem_uid(submission["problem"])
-            for submission in submissions
-            if submission["verdict"] == "OK"
-        }
+        return [
+            CrawledSubmission(
+                problem_url=self._generate_problem_url(sub),
+                verdict=(
+                    SubmissionVerdict.accepted
+                    if sub["verdict"] == "OK"
+                    else SubmissionVerdict.rejected
+                ),
+            )
+            for sub in submissions
+        ]
 
     @staticmethod
-    def generate_problem_uid(problem) -> SolutionUid:
-        contest_id = int(problem["contestId"])
-        problem_id = problem["index"]
+    def _generate_problem_url(submission: dict[str, Any]) -> str:
+        contest_id = int(submission["contestId"])
+        problem_id = submission["index"]
         contest_type = "gym" if contest_id > 100_000 else "contest"
         url = f"https://codeforces.com/{contest_type}/{contest_id}/problem/{problem_id}"
         return url
