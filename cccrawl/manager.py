6eb841c40843a787145c4ac9c848ac3c4f2dd7b8
Alon Krymgand <me@alon.kr>
2023-07-21 23:31:41 +0300

    Added partial implementation of new manager

--- cccrawl/manager.py
+++ cccrawl/manager.py
@@ -5,7 +5,7 @@ from httpx import AsyncClient
 
 from cccrawl.crawlers import CodeforcesCrawler, Crawler, CsesCrawler
 from cccrawl.db.base import Database
-from cccrawl.models.submission import CrawledSubmission
+from cccrawl.models.submission import CrawledSubmission, Submission
 from cccrawl.models.user import UserConfig
 
 logger = getLogger(__name__)
@@ -31,24 +31,45 @@ class MainCrawler:
         async def single_crawl(crawler: Crawler):
             submissions.extend(await crawler.crawl(user))
 
-        async def time_task():
-            await asyncio.sleep(WAIT_BETWEEN_CRAWLS)
-
         async with asyncio.TaskGroup() as tg:
-            tg.create_task(time_task())
             for crawler in self._crawlers:
                 tg.create_task(single_crawl(crawler))
 
         return submissions
 
+    async def crawl_user_and_update_db(self, user: UserConfig) -> None:
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(asyncio.sleep(WAIT_BETWEEN_CRAWLS))
+            all_submissions_task = tg.create_task(self.crawl_user_submissions(user))
+            old_submissions_task = tg.create_task(self._db.get_user_submissions(user))
+
+        all_submissions = all_submissions_task.result()
+        old_submissions = old_submissions_task.result()
+        new_submissions = self._generate_new_submissions(
+            all_submissions, old_submissions
+        )
+
+        return old_submissions + new_submissions
+
     async def crawl(self) -> None:
         async for user in self._db.generate_users():
             try:
-                logger.info("Started crawling user %s", user.uid)
-                await self.crawl_user(user)
+                await self.crawl_user_and_update_db(user)
             except Exception:
                 logger.error(
                     "Failed to crawl user %s, skipping to next user",
                     user.uid,
                     exc_info=True,
                 )
+
+    @classmethod
+    def _generate_new_submissions(
+        cls,
+        all_submissions: list[CrawledSubmission],
+        old_submissions: list[Submission],
+    ) -> list[Submission]:
+        """Receives a list of newly crawled submissions and the list of
+        previously crawled submissions, filters out all submissions that
+        already have been crawled, and returns only the list of new
+        submissions."""
+        raise NotImplementedError()
