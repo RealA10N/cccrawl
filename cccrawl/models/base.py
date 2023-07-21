31322c7a0336e2a5be6233689e3dfea36272d887
Alon Krymgand <me@alon.kr>
2023-07-21 13:13:38 +0300

    Added base enum

--- cccrawl/models/base.py
+++ cccrawl/models/base.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel
+from enum import StrEnum
 
 
 class CCBaseModel(BaseModel):
-    """ A Base model for all models used by CodeCoach and the crawler."""
+    """A base model for all models used by CodeCoach and the crawler."""
+
+
+class CCBaseStrEnum(StrEnum):
+    """A base string based enum that is used by all enums in CodeCoach and the
+    crawler."""
