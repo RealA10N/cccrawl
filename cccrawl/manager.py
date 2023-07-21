277081247a9b46a6ead1f685fa2b4a94a4523cd7
Alon Krymgand <me@alon.kr>
2023-07-21 22:26:49 +0300

    Updated crawl_user_submission method

--- cccrawl/manager.py
+++ cccrawl/manager.py
@@ -1,17 +1,16 @@
 import asyncio
 from logging import getLogger
-from typing import Type
 
 from httpx import AsyncClient
 
 from cccrawl.crawlers import CodeforcesCrawler, Crawler, CsesCrawler
 from cccrawl.db.base import Database
-from cccrawl.models.solution import SolutionUid
+from cccrawl.models.submission import CrawledSubmission
 from cccrawl.models.user import UserConfig
 
 logger = getLogger(__name__)
 
-ALL_CRAWLERS: list[Type[Crawler]] = [CodeforcesCrawler, CsesCrawler]
+ALL_CRAWLERS: list[type[Crawler]] = [CodeforcesCrawler, CsesCrawler]
 WAIT_BETWEEN_CRAWLS = 8
 
 
@@ -20,31 +19,33 @@ class MainCrawler:
         self._db = db
         self._crawlers = [crawler_cls(client) for crawler_cls in ALL_CRAWLERS]
 
-    async def crawl_user(self, user: UserConfig) -> set[SolutionUid]:
-        collected_per_crawler = list()
+    async def crawl_user_submissions(
+        self,
+        user: UserConfig,
+    ) -> list[CrawledSubmission]:
+        """Crawl submissions from all crawlers to the given user
+        synchronically."""
+
+        submissions = list()
 
         async def single_crawl(crawler: Crawler):
-            collected_per_crawler.append(await crawler.crawl(user))
+            submissions.extend(await crawler.crawl(user))
 
         async def time_task():
             await asyncio.sleep(WAIT_BETWEEN_CRAWLS)
 
-        tasks = set()
-        tasks.add(asyncio.create_task(time_task()))
-        for crawler in self._crawlers:
-            tasks.add(asyncio.create_task(single_crawl(crawler)))
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(time_task())
+            for crawler in self._crawlers:
+                tg.create_task(single_crawl(crawler))
 
-        for task in tasks:
-            await task
-
-        return set.union(*collected_per_crawler)
+        return submissions
 
     async def crawl(self) -> None:
         async for user in self._db.generate_users():
             try:
                 logger.info("Started crawling user %s", user.uid)
-                solutions = await self.crawl_user(user)
-                await self._db.store_user_solutions(user, solutions)
+                await self.crawl_user(user)
             except Exception:
                 logger.error(
                     "Failed to crawl user %s, skipping to next user",
