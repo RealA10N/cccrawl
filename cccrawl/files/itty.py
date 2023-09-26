935e5f6b66af9ea9f7e2d06eda9906b42f762a10
Alon Krymgand <me@alon.kr>
2023-09-27 00:49:19 +0300

    Updated file uploader protocol

--- cccrawl/files/itty.py
+++ cccrawl/files/itty.py
@@ -1,18 +1,19 @@
 from typing import TextIO
 
 from httpx import AsyncClient
-from yarl import URL
+from pydantic import HttpUrl
 
 from cccrawl.files.base import FileUploadService
 
 
 class IttyUploadService(FileUploadService):
-    async def upload(self, content: TextIO) -> URL:
+    async def upload(self, content: TextIO) -> HttpUrl:
         async with AsyncClient() as client:
             response = await client.post(
                 url="https://ity.sh/",
                 params={"ttl": "30years"},
                 json=content.read(),
             )
-        assert response.status_code == 200
-        return response.json()["url"]
+        response.raise_for_status()
+        url: str = response.json()["url"]
+        return HttpUrl(url)
