a9cad5d997ab5c16d0eae240232ef8b4317d37e2
Alon Krymgand <me@alon.kr>
2023-09-27 00:13:02 +0300

    Added abstract finalize new submission step

--- cccrawl/crawlers/base.py
+++ cccrawl/crawlers/base.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
 from collections.abc import AsyncIterable
 from logging import getLogger
-from typing import Any, Generic, TypeAlias, TypeVar
+from typing import Any, Coroutine, Generic, TypeAlias, TypeVar
 
 from httpx import AsyncClient
 
 from cccrawl.models.integration import Integration
-from cccrawl.models.submission import CrawledSubmission
+from cccrawl.models.submission import CrawledSubmission, Submission
 
 IntegrationT = TypeVar("IntegrationT", bound=Integration)
 
@@ -19,8 +19,26 @@ class Crawler(ABC, Generic[IntegrationT]):
 
     @abstractmethod
     def crawl(self, integration: IntegrationT) -> AsyncIterable[CrawledSubmission]:
-        """Given the configuration of the user, returns a list of all
-        submissions of the user."""
+        """Provided an integration, this method should crawl a subset of the
+        integration, in which it is guaranteed that all new submissions appear
+        in such subset. In particular, it is OK to crawl submissions that have
+        already been seen before (that have been reported with previous calls
+        to the same function, or that are already stored in the database).
+        The implementation logic, of how to filter out new submissions and
+        query them is left for to the implementation of the specific platform
+        crawlers."""
+
+    @abstractmethod
+    async def finalize_new_submission(
+        self, crawled_submission: CrawledSubmission
+    ) -> Coroutine[Submission, None, None]:
+        """Provided a new crawled submission with only partial data available,
+        this method should crawl additional data that is considered 'expensive',
+        and convert it into a full 'Submission' instance.
+        Note: This method is called only once per submission, before it is
+        inserted into the database. It may leave traces and cause side effects,
+        like for example, copying the source code of the submission into a
+        CodeCoach-managed database."""
 
 
 AnyCrawler: TypeAlias = Crawler[Any]
