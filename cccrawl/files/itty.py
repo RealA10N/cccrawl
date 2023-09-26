301713e74d873e3a5152ded5c8aceaa69c746b2b
Alon Krymgand <me@alon.kr>
2023-09-27 01:37:37 +0300

    Added arguments to itty constructor

--- cccrawl/files/itty.py
+++ cccrawl/files/itty.py
@@ -7,11 +7,17 @@ from cccrawl.files.base import FileUploadError, FileUploadService
 
 
 class IttyUploadService(FileUploadService):
+    def __init__(self, key_length: int = 8, time_to_live: str = "30years") -> None:
+        # WARNING: There is no argument validation here!
+        # not implemented since it is kind messy and time consuming.
+        self._key_length = key_length
+        self._time_to_live = time_to_live
+
     async def upload(self, content: TextIO) -> HttpUrl:
         async with AsyncClient() as client:
             response = await client.post(
                 url="https://ity.sh/",
-                params={"ttl": "30years"},
+                params={"ttl": self._time_to_live, "length": self._key_length},
                 json=content.read(),
             )
 
