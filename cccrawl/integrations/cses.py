ea7bbc581c44fc7be9d7fb519136d1ea55357f7c
Alon Krymgand <me@alon.kr>
2023-09-27 13:18:20 +0300

    Added handle to cses integrations

--- cccrawl/integrations/cses.py
+++ cccrawl/integrations/cses.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-from pydantic import computed_field, conint
+from pydantic import computed_field, conint, constr
 
 from cccrawl.models.base import ModelId
 from cccrawl.models.integration import Integration, Platform
@@ -8,6 +8,7 @@ from cccrawl.models.integration import Integration, Platform
 
 class CsesIntegration(Integration):
     platform: Literal[Platform.cses]
+    handle: constr(min_length=1, max_length=16, strip_whitespace=True)  # type: ignore[valid-type]
     user_number: conint(strict=True, gt=0, le=10_000_000)  # type: ignore[valid-type]
 
     @computed_field  # type: ignore[misc]
