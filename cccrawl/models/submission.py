5a9587d82b21bc6acabf58c2f94df6a6177f0539
Alon Krymgand <me@alon.kr>
2023-09-27 12:14:03 +0300

    Now it is required to use explicit submission objects per crawler

--- cccrawl/models/submission.py
+++ cccrawl/models/submission.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from enum import auto
 from typing import TypeVar
 
@@ -19,39 +20,13 @@ class SubmissionVerdict(CCBaseStrEnum):
     rejected = auto()
 
 
-class CrawledSubmission(CCBaseModel):
+class CrawledSubmission(ABC, CCBaseModel):
     """A model that describes a single submission, where all information can
     and should be provided in a single scrape."""
 
     integration: AnyIntegration
     problem: Problem
     verdict: SubmissionVerdict
-
-    # The time in which the solution was submitted at. None if the judge does
-    # not provide such information.
-    submitted_at: AwareDatetime | None = None
-
-    # A URL pointing to a webpage of the judge, which contains the solution,
-    # source code, verdict, etc. (if exists)
-    submission_url: HttpUrl | None = None
-
-    @computed_field  # type: ignore[misc]
-    @property
-    def id(self) -> ModelId:
-        return ModelId(
-            self._hash_tokens(
-                self.integration.root,
-                self.problem,
-                self.verdict,
-                # Submitted at is part of the hash: if unable to collect, all
-                # submissions to the same problem with the same problem will
-                # be treated the same. Otherwise, the hash of submissions
-                # submitted at different times will be different.
-                str(self.submitted_at),
-                # The same goes for submission URL.
-                str(self.submission_url),
-            )
-        )
 
 
 SubmissionT = TypeVar("SubmissionT", bound="Submission")
@@ -65,6 +40,14 @@ class Submission(CrawledSubmission):
     # The first time the scraper scraped the solution.
     # Should not be changed between scrapes (constant value).
     first_seen_at: AwareDatetime
+
+    # The time in which the solution was submitted at. None if the judge does
+    # not provide such information.
+    submitted_at: AwareDatetime | None = None
+
+    # A URL pointing to a webpage of the judge, which contains the solution,
+    # source code, verdict, etc. (if exists)
+    submission_url: HttpUrl | None = None
 
     # A URL pointing to a raw text file with the submission source code.
     raw_code_url: HttpUrl | None = None
