70ca98e6fa9c70e3cc54969053d0a903cdeda523
Alon Krymgand <me@alon.kr>
2023-10-01 12:56:09 +0300

    Updated main script

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import httpx
 from azure.cosmos.aio import CosmosClient
 from dotenv import load_dotenv
 
+from cccrawl.crawlers.codeforces import CodeforcesCrawler
+from cccrawl.crawlers.cses import CsesCrawler, CsesCredentials
+from cccrawl.crawlers.toolkit import CrawlerToolkit
 from cccrawl.db.cosmos import CosmosDatabase
+from cccrawl.files.itty import IttyUploadService
 from cccrawl.manager import MainCrawler
+from cccrawl.models.integration import Platform
 
 logging.basicConfig()
 
@@ -23,11 +28,32 @@ load_dotenv()
 
 async def main():
     async with httpx.AsyncClient() as http_client:
+        toolkit = CrawlerToolkit(
+            client=http_client,
+            file_uploader=IttyUploadService(key_length=16),
+        )
+
+        crawlers_mapping = {
+            Platform.cses: CsesCrawler(
+                toolkit=toolkit,
+                credentials=CsesCredentials(
+                    username=os.getenv("CSES_USERNAME"),
+                    password=os.getenv("CSES_PASSWORD"),
+                ),
+            ),
+            Platform.codeforces: CodeforcesCrawler(
+                toolkit=toolkit,
+            ),
+        }
+
         async with CosmosClient(
             os.getenv("COSMOS_ENDPOINT"), os.getenv("COSMOS_KEY")
         ) as cosmos_client:
             db = await CosmosDatabase.init_database(cosmos_client)
-            await MainCrawler(http_client, db).crawl()
+            await MainCrawler(
+                db=db,
+                crawlers=crawlers_mapping,
+            ).crawl()
 
 
 asyncio.run(main())
